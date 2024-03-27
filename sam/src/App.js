acd32b5bd872a924dfd69df72ef0d91b57740e93
samuel sami <saminew3919@gmail.com>
2024-03-27 09:49:22 -0700

    first commit

---
+++ sam/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Marquee from './Marquee'
+
+function App() {
+  return (
+    <div className="App">
+        <Marquee text=" Welcome ti horrorville" />
+    </div>
+  );
+}
+
+export default App;
